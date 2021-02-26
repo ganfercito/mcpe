f20ea4926e7cbb735de785bcdfe525e4c94a214e
Glitch (heavenly-motley-thistle) <none>
2021-02-26 12:41:23 +0000

    🌍👧 Checkpoint ./index.html:17863194/112 ./index.js:17863194/33

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ const mineflayer = require("mineflayer");
 
 //the config
 const bot = mineflayer.createBot({
-  host: "xJinMori2021.aternos.me", //Coloca entre las comillas, tu ip
+  host: "IP", //Coloca entre las comillas, tu ip
   port: 25565, //puerto del servidor
-  username: "xJin2", //nombre del bot (CAMBIAR)
+  username: "Bot", //nombre del bot (CAMBIAR)
   version: false //version of the server (false = auto detect)
 });
 
